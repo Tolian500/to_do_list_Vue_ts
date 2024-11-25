aedea796ab54f01ba3818848c71d259a6920f436
tolian500 <tolian500@gmail.com>
2024-11-25 11:54:58 +0100

    setting Nav and Hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="bg-yellow-400 py-20 mb-4">
+      <div
+        class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
+      >
+        <div class="text-center">
+          <h1
+            class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
+          >
+            Can't remember? Write it!
+          </h1>
+          <p class="my-4 text-xl text-white">
+            Write it ones and you'll never ever forget it agian (c) Anatolii
+          </p>
+        </div>
+      </div>
+    </section>
+
+</template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -43,28 +43,24 @@
       </div>
     </nav> -->
 
-    <nav class="bg-green-700 border-b border-green-500">
+    <nav class="bg-yellow-500 border-b border-yellow-100">
       <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div class="flex h-20 items-center justify-between">
           <div
             class="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
+
             <div class="md:ml-auto">
               <div class="flex space-x-2">
                 <a
                   href="index.html"
-                  class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  class="text-white bg-yellow-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >Home</a
                 >
                 <a
-                  href="jobs.html"
-                  class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</a
-                >
-                <a
                   href="add-job.html"
-                  class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</a
+                  class="text-white hover:bg-yellow-700 hover:text-white rounded-md px-3 py-2"
+                  >Add To-Do List</a
                 >
               </div>
             </div>
